c3988354ccad03c150572248c6556b70a7e30c37
muhendra <eka_muhendra@yahoo.com>
2025-06-02 14:34:22 +0700

    changes pathasset

--- e-Pas_CMS/Helpers/ReportExcellentTemplate.cs
+++ e-Pas_CMS/Helpers/ReportExcellentTemplate.cs
@@ -288,7 +288,7 @@ public class ReportExcellentTemplate : IDocument
                         try
                         {
                             var relativePath = foto.Path.TrimStart('/');
-                            fullPath = Path.Combine("/var/www/epas-api/wwwroot", relativePath);
+                            fullPath = Path.Combine("/var/www/epasasset/wwwroot", relativePath);
                         }
                         catch
                         {
